93a21caeba6ce289faf75c52c22ffcfa1dce72aa
Rae Abunahla <raghebabn@gmail.com>
2021-09-27 15:01:17 -0400

    resume test

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -21,11 +21,11 @@ export function NaviBar(props) {
                 </div>
                 <ul class="menu">
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
+                <li><a href="/#about">About</a></li>
                 <li><a href="/design-projects"> Design </a></li>
                 <li><a href="/code-projects"> Code </a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a onclick="window.open('./docs/Resume-Rae-Abunahla.pdf','_blank')" class="resume">Resume</a></li>
+                {/* <li><a href="mailto:rabg">Contact</a></li> */}
+                <li><a href="/docs/rae-one-pager.pdf" target="_blank" class="resume">Resume</a></li>
                 </ul>
                 <div className="menu-btn">
                 <i class="fas fa-bars"></i>
